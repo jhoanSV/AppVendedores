5b54fe70ef274580cfd7508eccec0bb500afc9e5
jhoanSV <jhoanjssv97@outlook.com>
2022-09-08 15:03:08 -0500

    sesión y producto
    se habilito el iniciar sesión sin validación de datos y la muestra del detalle del producto que se escoja.

---
+++ src/componentes/DetalleProducto.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button} from 'react-native';
+import Layout from '../components/Layout';
+import { Icon } from 'react-native-elements'
+
+function DetalleProducto({ navigation, route }) {
+  return (
+    
+    <View style={styles.container}>
+      <Icon name='west'onPress={() => navigation.goBack()} />
+      <Text style={styles.subTitle}>Cod:</Text>
+      <Text style={styles.text}>{route.params.cod}</Text>
+      <Text style={styles.subTitle}>Descripción:</Text>
+      <Text style={styles.text}>{route.params.Descripcion}</Text>
+      <Text style={styles.subTitle}>Empaque:</Text>
+      <Text style={styles.text}>{route.params.UnidadOpaquete}</Text>
+      <Text style={styles.text}>{route.params.EsUnidadOpaquete}</Text>
+      <Text style={styles.subTitle}>Sub categoria:</Text>
+      <Text style={styles.text}>{route.params.SubCategoria}</Text>
+      <Text style={styles.subTitle}>Precio de venta:</Text>
+      <Text style={styles.text}>$ {route.params.PVenta}</Text>
+      <Text style={styles.subTitle}>Nota:</Text>
+      <Text style={styles.text}>{route.params.Nota}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container : {
+    padding: 10,
+  },
+  subTitle: {
+    fontSize: 20, 
+    fontWeight: 'bold', 
+    margin: 8,
+    color: 'black',
+  },
+  text: {
+    fontSize: 16,  
+    marginLeft: 8,
+    color: 'black',
+  },
+});
+
+export default DetalleProducto;
--- src/components/TasksItem.jsx
+++ src/components/TasksItem.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-
+import { TouchableOpacity, onLongPress } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const TasksItem = ({ task }) => {
+
+  const navigation = useNavigation()
   return (
+    <TouchableOpacity onPress={() => navigation.navigate('DetalleProducto', {cod: task.cod, Descripcion: task.Descripcion, UnidadOpaquete: task.UnidadOpaquete, EsUnidadOpaquete: task.EsUnidadOpaquete,SubCategoria: task.SubCategoria,  PVenta: task.PVenta, Nota: task.Nota})}>
     <View style={styles.itemContainer}>
-      <Text style={[styles.itemText, {width: 80}]}>{task.cod}</Text>
-      <Text style={[styles.itemText, {width: 400}]}>{task.Descripcion}</Text>
-      <Text style={[styles.itemText, {width: 55}]}>{task.UnidadOpaquete}</Text>
-      <Text style={[styles.itemText, {width: 40}]}>{task.EsUnidadOpaquete}</Text>
-      <Text style={[styles.itemText, {width: 200}]}>{task.SubCategoria}</Text>
-      <Text style={[styles.itemText, {width: 100}]}>$ {task.PVenta}</Text>
-      <Text style={styles.itemText}>{task.Nota}</Text>
+        <Text style={[styles.itemText, {width: 80}]}>{task.cod}</Text>
+        <Text style={[styles.itemText, {width: 400}]}>{task.Descripcion}</Text>
+        <Text style={[styles.itemText, {width: 55}]}>{task.UnidadOpaquete}</Text>
+        <Text style={[styles.itemText, {width: 40}]}>{task.EsUnidadOpaquete}</Text>
+        <Text style={[styles.itemText, {width: 200}]}>{task.SubCategoria}</Text>
+        <Text style={[styles.itemText, {width: 100}]}>$ {task.PVenta}</Text>
+        <Text style={styles.itemText}>{task.Nota}</Text>
     </View>
+    </TouchableOpacity>
   )
 };
 
