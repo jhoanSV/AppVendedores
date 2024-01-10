93b4fce37a10f049727c17bd25c34c515840b48c
jhoanSV <jhoanjssv97@outlook.com>
2024-01-10 11:51:42 -0500

    Products and log in
    Changes in the Backend to find the list of products depending on if the user is logged in or not, and a prototype of function to check the log in.

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -1,5 +1,19 @@
 import { Router } from 'express';
-import { getTasks, ValidarDatos, BuscarClientesTodos, aTablas, consecutivos, DatosProgreso, PedidosEnviados, DetalleDelPedidoVendedor, PedidosCerrados, PedidosPorEntregar, DetallePedidoEntregas, ActualizarProcesoDelPedido, DetallePedidoCerrado } from '../controllers/tasks';
+import { getTasks, 
+        ValidarDatos, 
+        BuscarClientesTodos, 
+        aTablas, 
+        consecutivos, 
+        DatosProgreso, 
+        PedidosEnviados, 
+        DetalleDelPedidoVendedor, 
+        PedidosCerrados, 
+        PedidosPorEntregar, 
+        DetallePedidoEntregas, 
+        ActualizarProcesoDelPedido, 
+        DetallePedidoCerrado,
+        ProductDataForNotLoggedInClient, 
+        ProductDataForLoggedInClient} from '../controllers/tasks';
 
 
 const router = Router();
@@ -70,5 +84,11 @@ router.get('/tasks/DetallePedidoEntregas/:cod', DetallePedidoEntregas)
 router.post('/tasks/ActualizarProcesoDelPedido', ActualizarProcesoDelPedido)
 
 router.get('/tasks/DetallePedidoCerrado/:cod', DetallePedidoCerrado)
+
+router.get('/tasks/proNotLoggedUser', ProductDataForNotLoggedInClient)
+
+router.get('/tasks/proLoggedInUser', ProductDataForLoggedInClient)
+
+
 
 export default router
